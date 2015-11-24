bcca2470cdf495dbd211ddde0c9dec20f29d86ae
RollinTschirgi <rtschirgi16@cornellcollege.edu>
2015-11-24 11:06:35 -0600

    Update schedules.c

--- week0/rollin/src/schedules.c
+++ week0/rollin/src/schedules.c
@@ -1,63 +1,78 @@
+// "..it is the chief characteristic of the religion of science, that it works.."
+//                                                      -Issac Asimov, Foundation(1951)
+//Rollin Tschirgi
+//CSC311
+//23/Nov/2016
 
-// schedules.c
-// Norbert Wiener
-// CSC311 Systems Software
-// 23 November 2015
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<math.h>
+
+//mean-service-time
+#define MST 2.0
+//mean-inter-arrival-time
+#define MIAT 3.0
+
+typedef struct process ProcessRecord, *ProcessRecordPointer;
+
+//define a node in a linked list defining a process
+//it will contain:
+//time needed to complete a process
+//time until next process starts
+struct process{
+  int id;
+  double timeToService;
+  double timeUntilNextProcess;
+  ProcessRecordPointer np;
+}; //struct
+
+//Globals####################################
+ProcessRecordPointer rootPointer = NULL;
+ProcessRecordPointer pp = NULL;
+ProcessRecordPointer cp = NULL;
+//###########################################
+
+int main(int argc, char** argv){
+
+//time is measured in seconds
+//t is 64-bit
+time_t t = time(NULL);
+//seed with random number generator
+srand(t);
 
 
-// An illustration of several features
-// of the C programming language.
-//   1) preprocessor directives (#include and #define)
-//   2) header files (stdio.h, stdlib.h, time.h, math.h)
-//   3) typedefs
-//   4) structs
-//   5) library functions (malloc(), srand(), rand(), log())
-//   6) casts 
-//   7) pointers
 
-#include <stdio.h>
-#include <stdlib.h>
-
-// define a node in a linked list
-// that describes a process (a task)
-// this description includes:
-//   1) the time required to complete the process
-//   2) the time until the next process joins the queue
-
-int main( int argc, char **argv ) {
-
-  // Seed the random number generator
-  // with the time measured in seconds.
-  // "time_t" is a just another name for
-  // a long (64 bit) integer.
-
-  // NULL is a constant defined for us.
-  // (It is zero.)
-
-  // Must declare counter before beginning loop!
-
-    // malloc() is the function for "memory allocation"
-    // sizeof is an operator that tells us how many bytes
-    // we'll need to store an object of a given type.
-    // malloc() returns a general kind of pointer (void *)
-    // so we must cast it to remind the compiler to what 
-    // kind of object we are pointing.
-
-    // draw a random number from an exponential distribution
-    // with a specified mean
-
-    // draw a random number from an exponential distribution
-    // with a specified mean
+int i;
+for(i = 0; i < 12; i++){
     
-    // add this process to the list of processes
-    // (this is the "linking" part of the linked list)
+    cp = 
+    (ProcessRecordPointer) //cast as PRP
+    malloc( //allocates a certain amount of memory and returns a pointer to the memory specified
+    sizeof(ProcessRecord)); //returns the size needed to store the process record
+    cp->id = i;//set id to 1 at location cp
+    
+    double r = ((double) rand())/RAND_MAX;
+    cp->timeToService = -MST * log(r);
+    
+    r = ((double) rand())/RAND_MAX;
+    cp->timeUntilNextProcess = -MIAT * log(r);
+    
+    cp->np=pp;
+    pp=cp;
+  }//for
+  rootPointer = cp;
+  cp = rootPointer;
+  while(cp != NULL){
+  
+  printf("process id = %d \n", cp->id);
+  printf("\t service time = %8.4f \n", cp->timeToService);
+  printf("\t interarrival time = %8.4f \n", cp->timeUntilNextProcess);
+  cp = cp->np;
+  
+  }//while
 
-  // traverse the list.
-  // begin at the last element added to the list
-  // and end with the first element we added to the list.
-
-  printf( "Hello from schedules!\n" );
-
-  exit(0);  
-} // main( int, char** )
-
+printf("..it is the chief characteristic of the religion of science, that it works.. \n                                         -Issac Asimov, Foundation(1951)");
+//                                                      
+exit(0);
+}//main
