da36c7986b0d3089b0ed8b5570e8a752ed7a56d7
RollinTschirgi <rtschirgi16@cornellcollege.edu>
2015-11-24 11:47:36 -0600

    finished all but main method

--- week0/rollin/src/treesort.c
+++ week0/rollin/src/treesort.c
@@ -1,6 +1,6 @@
 
 // treesort.c
-// Norbert Wiener
+// Rollin Tschirgi
 // CSC311 Systems Software
 // 23 November 2015
 
@@ -8,22 +8,59 @@
 #include <stdlib.h>
 
 // define a node in a binary search tree
+struct node{
+    int value;
+    struct node* lp;
+    struct node* rp;
+};//node
 
 // define aliases
-
+deftype struct node Node;
+deftype struct node* NodePtr;
 // define a function that will create
 // a new tree that contains only a single
 // node
+NodePtr createRoot(int n){
+    NodePtr np = (NodePtr)malloc(sizeof(Node));
+    np->value = n;
+    np->lp = NULL;
+    np->rp = NULL;
+    return np;
+}//createroot
 
 // define a function that will add a new
 // node to an existing binary search tree
-
+NodePtr addNode(NodePtr np, int n){
+    NodePtr rp = np;
+    
+    if(np==NULL){rp = createRoot(n);}
+    else if(n < np->value){
+       
+        if(np->lp == NULL){np->lp = createRoot(n);}
+        else{np->lp = addNode(np->lp, n);}
+    }
+    else if(n > np->value){
+       
+        if(np->rp==NULL){np->rp = createRoot(n);}
+        else{np->rp = addNode(np->rp, n);}
+    }
     // if value to be added is already in tree, do nothing
     // (do not add duplicate values to tree)
+    else{}
+}//addnode
+
 
 // traverse a binary search tree
 // in order, printing the values
 // found at each node
+void printNodes(NodePtr np){
+    if(np != NULL){
+        printNodes(np->lp);
+        printf("value at node = %4d \n", np->value);
+        printNodes(np->rp);
+    }
+}
+
 
 int main( int argc, char** argv ) {
 
